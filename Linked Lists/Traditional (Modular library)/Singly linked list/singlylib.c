4f408a85098681ff97bb311a60bcea9baf6bab8d
Rafael Henrique Martinez <128187479+BlackCattowo@users.noreply.github.com>
2024-10-24 16:19:34 -0300

    Given discriptions to list

--- Linked Lists/Traditional (Modular library)/Singly linked list/singlylib.c
+++ Linked Lists/Traditional (Modular library)/Singly linked list/singlylib.c
@@ -18,14 +18,11 @@ void insert_at(singly *list, int index, float data)
 {
     Node *new_node = create_node(data);
 
-    // Case 1: Insert at the head (position 0)
     if (index <= 0 || list->size == 0) 
     {
         new_node->next = list->head;
         list->head = new_node;
     }
-    // Case 2: Insert somewhere else
-    else 
     {
         Node *current = list->head;
         for (int i = 0; i < index - 1 && current->next != NULL; i++) 
@@ -37,7 +34,6 @@ void insert_at(singly *list, int index, float data)
         current->next = new_node;
     }
 
-    // Increase the size of the list
     list->size++;
 }
 
@@ -50,13 +46,11 @@ void remove_at(singly *list, int index)
 
     Node *current = list->head;
 
-    // Case 1: Remove head
     if (index == 0) 
     {
         list->head = current->next;
         free(current);
     }
-    // Case 2: Remove from somewhere else
     else 
     {
         Node *prev = NULL;
@@ -69,7 +63,6 @@ void remove_at(singly *list, int index)
         free(current);
     }
 
-    // Adjust the size
     list->size--;
 }
 
@@ -87,7 +80,7 @@ int get_node(singly *list, float data)
     if (current == NULL) 
     {
         printf("Element not found.\n");
-        return -1;  // Return -1 when not found
+        return -1;  
     }
     
     return index;
@@ -98,7 +91,7 @@ float get_element_at(singly *list, int index)
     if (index < 0 || index >= list->size) 
     {
         printf("Index out of bounds.\n");
-        return -1;  // Return -1 for invalid index
+        return -1;  
     }
 
     Node *current = list->head;
@@ -112,7 +105,7 @@ float get_element_at(singly *list, int index)
 
 void free_list(singly *list) 
 {
-    if (list->size == 0) return;  // No need to free if the list is empty
+    if (list->size == 0) return;  
 
     Node *current = list->head;
     Node *next_node;
